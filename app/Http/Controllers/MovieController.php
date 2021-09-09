e647c93c086ecfb6ab09c15791f802ecd5c00ecf
NicolaRebecca <master9895@gmail.com>
2021-09-09 16:22:37 +0200

    db not load

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all(); 
+        return view('/home', compact('movies'));
+
+        
+    }
+
 }
